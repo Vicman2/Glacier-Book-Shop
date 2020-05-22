9d1d7f2f4e24f3f4d77117db1ccb428abe8147e5
Vicman2 <vicmanthebest@gmail.com>
2020-05-22 22:52:04 +0000

    Logic for looping into the books, enhancing UI and making the navbar fixed

--- src/components/BookInHome/BookInHome.js
+++ src/components/BookInHome/BookInHome.js
@@ -4,15 +4,22 @@ import { connect } from 'react-redux'
 
 
 const BookInHome = (props) => {
-    const src = props.ImageEndPoint + props.imageUrl
+    const src = props.imageEndPoint + props.imageUrl
+    const titleCase = (str) => {
+        let splitString = str.split(" ")
+        for(let i = 0; i < splitString.length; i++){
+            splitString[i] = splitString[i].charAt(0).toUpperCase() + splitString[i].substring(1)
+        }
+        return splitString.join(' ')
+    }
     return(
         <div className="BookInHome">
             <section className="BookInHome_Image_Container">
                 <img src={src} alt="" />
             </section>
             <section className="BookInHome_Detail">
-                <p className="BookInHome_Title"> {props.title} </p>
-                <p className="BookInHome_Author">{props.author} </p>
+                <p className="BookInHome_Title"> {titleCase(props.title)} </p>
+                <p className="BookInHome_Author">{titleCase(props.author)} </p>
             </section>
         </div>
     )
--- src/container/Home/Home.js
+++ src/container/Home/Home.js
@@ -23,28 +23,22 @@ class Home extends Component{
         let booksToSlide = [];
         let booksForCard = [];
         let totalBook = []
-        let booksToDisplay = [];
+        let booksToDisplay = <div></div>;
         if(!this.props.data.loading){
-            
             booksToSlide = this.state.books.filter((book, index) => index <15)
             booksToSlide = booksToSlide.reverse()
             booksForCard= this.state.books.filter((book, index) => index > 3 && index <=6)
             booksForCard = booksForCard.reverse()
             totalBook = [...this.state.books];
-            for (let i = 0; i < totalBook.length; i += 6) {
-                let chunk = totalBook.slice(i, i + 6)
-                totalBook.push(chunk)
-            }
-            totalBook.map(book => {
-                console.log(book.author)
-                // return(
-                //     // <BookInHome
-                //     //     key={book._id}
-                //     //     imageUrl={book.imageUrl}
-                //     //     title={book.title}
-                //     //     author={book.author.name}
-                //     // />
-                // )
+            booksToDisplay =  this.state.books.map((book,index) => {
+                return(
+                    <BookInHome
+                        key={book._id}
+                        imageUrl={book.imageUrl}
+                        title={book.title}
+                        author={book.author.name}
+                    />
+                )
             })
         }
 
@@ -55,11 +49,11 @@ class Home extends Component{
                 <div className="Category">
                     <p>Category</p>
                     <div className="Category_Image_Container">
-                       {booksToDisplay}
+                       <h1>  </h1>
                     </div>
                 </div>
                 <div className="Our_Books">
-                    
+                    {booksToDisplay}
                 </div>
             </Aux>
         )
