e8e4307ba6f06ff2ada5d0a9598eee039253f559
Vicman2 <vicmanthebest@gmail.com>
2020-07-09 10:07:41 +0000

    Debugged url

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ import * as serviceWorker from './serviceWorker';
 import { getInLocalStorage } from './Util/localStorage';
 
 const httpLink = createHttpLink({
-  uri: 'http://localhost:5000/graphql/',
+  uri: 'https://glacier-api.herokuapp.com/graphql/',
   credentials: 'include' 
 })
 
